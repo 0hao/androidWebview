fe40fc4d0cbd964b38060c532dead01847eebace
linhao <linhao87@gmail.com>
2013-04-11 18:46:37 +0800

    多余abc模块

--- CaseWebview/assets/www/js/app/src/a.js
+++
@@ -1,8 +0,0 @@
-define(function(require, exports, module) {
-    var $ = require('jquery');
-    function modelA(text) {
-        $('<p>model A load</p>').appendTo('#list');
-    }
-
-    module.exports = modelA;
-});
--- CaseWebview/assets/www/js/app/src/c.js
+++
@@ -1,8 +0,0 @@
-define(function(require, exports, module) {
-    var $ = require('jquery');
-    function modelC(text) {
-        $('<p>&nbsp;&nbsp;--model C load</p>').appendTo('#list');
-    }
-
-    module.exports = modelC;
-});
